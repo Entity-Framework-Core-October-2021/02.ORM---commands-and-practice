d52da87c80354a4bde715e6d6652d26e63f5ad94
Remzi <remzitdf@abv.bg>
2021-10-28 09:06:53 +0300

    Add files via upload

---
+++ 02.CodeFirstDemo/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.CodeFirstDemo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Integrated Security=true;Database=CodeFirstDemo2021");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ 02.CodeFirstDemo/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace _02.CodeFirstDemo.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.News = new HashSet<News>();
+        }
+        [Key]
+        public int Id { get; set; }
+        
+        [MaxLength(100)]
+        [Required]
+        public string Title { get; set; }
+        public virtual ICollection<News> News { get; set; }
+    }
+}
