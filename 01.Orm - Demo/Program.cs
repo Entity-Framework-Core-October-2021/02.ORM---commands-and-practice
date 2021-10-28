a25b5ab77308a4a1dcca0da915c065d24c8de155
Remzi <remzitdf@abv.bg>
2021-10-28 09:05:30 +0300

    Add files via upload

---
+++ 01.Orm - Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using _02.Orm___Demo.Models;
+using System;
+using System.Linq;
+
+namespace _02.Orm___Demo
+{
+    class Program
+    {
+        //1.NuGet Packages
+        //Microsoft.EntityFrameworkCore.SqlServer
+        //Microsoft.EntityFrameworkCore.Design
+
+        //2.Commands in Terminal:
+        //dotnet tool install --global dotnet-ef
+        //dotnet ef dbcontext scaffold "Server=.;Integrated Security=true;Database=SoftUnii" Microsoft.EntityFrameworkCore.SqlServer -o Models
+        static void Main(string[] args)
+        {
+            var db = new SoftUniiContext();
+            Console.WriteLine(db.Employees.Count());
+
+            //db.Towns.Add(new Town { Name = "Pernik" });
+            //db.SaveChanges();
+
+            var employees = db.Employees.Where(x => x.FirstName.StartsWith("N"))
+                                        .OrderByDescending(x => x.Salary)
+                                        .Select(x => new { x.FirstName, x.LastName, x.Salary })
+                                        .ToList();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} => {employee.Salary}");
+            }
+        }
+    }
+}
